4f05681fbd0ec6c8e9c0507349921ab705cf3e0b
Kalango! <kalani_potolowsky@estudante.sc.senai.br>
2025-07-07 22:46:29 -0300

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Header.css'; 
 
 const PawLogo = () => (
-  <svg className="logo-svg" viewBox="0 0 512 512" fill="currentColor">
-    <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0 0 114.6 0 256s114.6 256 256 256zM159.3 192c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm192 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zM256 320c-44.2 0-80-17.9-80-40s35.8-40 80-40 80 17.9 80 40-35.8 40-80 40z"/>
-  </svg>
-);
+    <svg className="logo-svg" viewBox="0 0 200 200" fill="currentColor">
+      <circle cx="50" cy="55" r="25" />
+      <circle cx="150" cy="55" r="25" />
+      <circle cx="100" cy="35" r="25" />
+      <path d="M 50,100 C 50,70 150,70 150,100 C 170,140 125,165 100,165 C 75,165 30,140 50,100 Z" />
+    </svg>
+  );
 
 
 function Header({ user, onReportClick, onLoginClick, onLogoutClick }) {
